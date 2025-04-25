cee8df510541b20fa297bf5052488f5c1e215932
Susan <susanbot@gmail.com>
2025-04-25 23:08:45 +0100

    lab week 08 part 01

---
+++ week08-plotting/activity01_salaries copy.py
@@ -0,0 +1,27 @@
+# A program that makes a list, called salaries, that has (say 10) random numbers (20000 – 80000)
+
+# Author: Susan Collins
+
+# import the NumPy library 
+import numpy as np
+
+# set parameters for salary generation
+salary_min = 20000
+salary_max = 100000
+n_salaries = 10
+
+# Seed the random number generator, for debugging purposes
+np.random.seed(1)
+
+# generate the list of salaries
+salaries = np.random.randint(salary_min, salary_max,10)
+
+print (salaries)
+
+# create list containing salaries increased by 5000
+salaries_increased = salaries + 5000
+print (salaries_increased)
+
+# create list containing salaries increased by 5%
+salaries_percent_increase = salaries * 1.05
+print (salaries_percent_increase)
